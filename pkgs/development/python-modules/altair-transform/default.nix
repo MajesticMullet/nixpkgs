314ec8a80799d728743ee35a2990856ff4c67771
Tom Ho <thomas.ho@formbay.com.au>
2020-11-09 09:09:15 +1100

    Adding default.nix

---
+++ pkgs/development/python-modules/altair-transform/default.nix
@@ -0,0 +1,41 @@
+{ buildPythonPackage
+, fetchPypi
+, ply
+, altair
+, numpy
+, pandas
+, dataclasses
+, pytest
+, jinja2
+, lib
+, pythonOlder
+}:
+
+buildPythonPackage rec {
+  pname = "altair_transform";
+  version = "0.2.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0rid432fqslaaqjnpd4fm5hxq88160w5yqr6qsm4n9bi795cjj5k";
+  };
+
+  propagatedBuildInputs = [
+    ply
+    altair
+    numpy
+    pandas
+  ] ++ lib.optionals (pythonOlder "3.7") [ dataclasses ];
+
+  checkInputs = [
+    jinja2
+    pytest
+  ];
+
+  meta = {
+    homepage = "https://github.com/altair-viz/altair-transform";
+    description = "Python evaluation of Altair/Vega-Lite transforms";
+    license = lib.licenses.mit;
+  };
+
+}
